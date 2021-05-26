75ee4398e1b167c6a207e61241d46d9001e53bba
techieshubham30 <82032825+techieshubham30@users.noreply.github.com>
2021-05-26 13:17:23 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ var btnTranslate=document.querySelector("#btn-translate");
 
 var txtInput=document.querySelector("#txt-input");
 
-var outputDiv=document.querySelector("#output")
+var outputDiv=document.querySelector("#output");
+
+var serverURL="	https://api.funtranslations.com/translate/minion.json";
+
+function errorHandler(error){
+    console.log("error occured,error");
+}
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
 
 
 btnTranslate.addEventListener("click",function clickHandler(){
-    outputDiv.innerText= `bannana language : ${txtInput.value}`;
+    var inputText=txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
+    .catch(errorHandler)
 })
+
+
